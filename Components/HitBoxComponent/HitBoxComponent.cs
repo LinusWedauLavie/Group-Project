7ca708b11321e2102839a628abe57c2b9a3e1144
LinusWedauLavie <Linus-Milan.Wedau@bvb.lavie-reha.de>
2024-11-18 14:17:52 +0100

    options menu WIP, component update

--- Components/HitBoxComponent/HitBoxComponent.cs
+++ Components/HitBoxComponent/HitBoxComponent.cs
@@ -1,15 +1,14 @@
 using Godot;
 using System;
-
+ 
 public partial class HitBoxComponent : Area2D
 {
-	[Export] HealthComponent healthComponent;
-
-/*
-	public void damage(int attackDamage){
-		if (healthComponent != null){
-			healthComponent.takeDamage(attackDamage);
-		}
-	}
-*/
+    [Export] HealthComponent healthComponent;
+ 
+    public void GiveDamage(int attackDamage){
+        if (healthComponent != null){
+            healthComponent.TakeDamage(attackDamage);
+        }
+    }
 }
+
