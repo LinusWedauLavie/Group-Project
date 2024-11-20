88962872d05a8b61e2f40c086eb131dafffdd7b0
666Keast <keanu.zimolong@bvb.lavie-reha.de>
2024-11-20 13:00:23 +0100

    CamSwitchWIP

--- Components/HitBoxComponent/HitBoxComponent.cs
+++ Components/HitBoxComponent/HitBoxComponent.cs
@@ -6,7 +6,6 @@ public partial class HitBoxComponent : Area2D
     [Export] HealthComponent healthComponent;
  
     public void GiveOwnDamage(int attackDamage){
-            GD.Print("gfs");
         if (healthComponent != null){
             healthComponent.TakeDamage(attackDamage);
         }
