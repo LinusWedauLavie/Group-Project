fbada470b3eb34e2e969616eef64a89a9b54c549
666Keast <keanu.zimolong@bvb.lavie-reha.de>
2024-11-25 14:26:31 +0100

    Mapgenerator WIP

--- Map/CamSwitch.cs
+++ Map/CamSwitch.cs
@@ -7,56 +7,67 @@ public partial class CamSwitch : Node2D
     public CharacterBody2D player;
     public Vector2 addPlayerPosition;
     public Vector2 addCameraPosition;
+    public Timer timer;
+    public bool isRoomCleared = false;
+    public bool ichdarfdurchdietür = true;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         player = GetParent().GetNode<CharacterBody2D>("/root/Map/Player");
         mainCam = GetParent().GetNode<Camera2D>("/root/Map/MainCam");
-        GD.Print("sdfkj");
     }
 
 
     public void BodyEntered(Node2D area, string test)
     {
-        //TODO es übergibt das, was in die area2d rein gkommen ist und nicht welche border ausgelöst wurde
-        GD.Print("body entered: " + test); 
-
-        switch (test)
+        isRoomCleared = true;
+        GD.Print(area.Name + test);
+        if (isRoomCleared)
         {
-            case "LeftBorder":
-                addCameraPosition.X = -576;
-                addCameraPosition.Y = 0;
-                mainCam.Position += addCameraPosition;
-                addPlayerPosition.X = -40; 
-                player.GlobalPosition += addPlayerPosition;
-                break;
-            case "TopBorder":
-                addCameraPosition.Y = -324;
-                addCameraPosition.X = 0;
-                mainCam.Position += addCameraPosition;
-                addPlayerPosition.Y = -40; 
-                player.GlobalPosition += addPlayerPosition;
-                break;
-            case "RightBorder":
-                addCameraPosition.X = 576;
-                addCameraPosition.Y = 0;
-                mainCam.Position += addCameraPosition;
-                addPlayerPosition.X = 40; 
-                player.GlobalPosition += addPlayerPosition;
-                break;
-            case "BottomBorder":
-                addCameraPosition.Y = 324;
-                addCameraPosition.X = 0;
-                mainCam.Position += addCameraPosition;
-                addPlayerPosition.Y += 40; 
-                player.GlobalPosition += addPlayerPosition;
-                break;
+            switch (test)
+            {
+                case "LeftBorder":
+                    addCameraPosition.X = -576;
+                    addCameraPosition.Y = 0;
+                    GD.Print("x -576");
+                    mainCam.Position += addCameraPosition;
+                    addPlayerPosition.X = -160; 
+                    player.GlobalPosition += addPlayerPosition;
+
+                    break;
+                case "TopBorder":
+                    addCameraPosition.Y = -324;
+                    addCameraPosition.X = 0;
+                    GD.Print("y -324");
+                    mainCam.Position += addCameraPosition;
+                    addPlayerPosition.Y = -100; 
+                    player.GlobalPosition += addPlayerPosition;
+
+                    break;
+                case "RightBorder":
+                    addCameraPosition.X = 576;
+                    GD.Print("x 576");
+                    addCameraPosition.Y = 0;
+                    mainCam.Position += addCameraPosition;
+                    addPlayerPosition.X = 160; 
+                    player.GlobalPosition += addPlayerPosition;
+
+                    break;
+                case "BottomBorder":
+                    addCameraPosition.Y = 324;
+                    addCameraPosition.X = 0;
+                    GD.Print("y 324");
+                    mainCam.Position += addCameraPosition;
+                    addPlayerPosition.Y += 100; 
+
+                    break;
+            }
         }
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+
     }
 }
--- Map/MapGenerator.cs
+++ Map/MapGenerator.cs
@@ -1,63 +1,91 @@
 using Godot;
 using System;
-
-
 public partial class MapGenerator : Node2D
 {
-	public Node2D lastRoom;
-	public Node2D shouldPlacedRoom;
-	public Node2D Room1;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Room1 = (Node2D)ResourceLoader.Load<PackedScene>("res://Map/Scenes/Room1.tscn").Instantiate();
-		lastRoom = GetNode<Node2D>("StartRoom");
-		shouldPlacedRoom = Room1;
-		Generator();
-		
-	}
-		
-	public void Generator ()
-	{
-		Random random = new Random();
-		
-		byte[] seed = new byte[10];
-		random.NextBytes(seed);
-		
-		if (Convert.ToInt64(seed[0].ToString()) < 575)
-		{
-			Vector2I oben = new Vector2I();
-			oben.X = 0;
-			oben.Y = -324;
-			shouldPlacedRoom.Position = lastRoom.Position + oben;
-			lastRoom.AddChild(shouldPlacedRoom);
-			lastRoom = shouldPlacedRoom;
-			
-			
-		}
-		if(Convert.ToInt64(seed[2].ToString()) <= 128)
-		{
-			
-			Vector2I left = new Vector2I();
-			left.X = 576;
-			left.Y = 0;
-			shouldPlacedRoom.Position = lastRoom.Position + left;
-			lastRoom.AddChild(shouldPlacedRoom);
-			lastRoom = shouldPlacedRoom;
-		}
-		if(Convert.ToInt64(seed[2].ToString()) < 128)
-		{
-			//Dann soll ein raum nach rechts gebaut werden
-		}
-		if(Convert.ToInt64(seed[3].ToString()) < 128)
-		{
-			//Dann soll ein raum nach unten gebaut werden
-		}
-	}
+    public Node2D lastRoom;
+    public PackedScene shouldPlacedRoom;
+    public PackedScene Room1;
 
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+		Node2D test = (Node2D)GetParent().GetNode("StartRoom");
+		lastRoom = test;
+		Room1 = ResourceLoader.Load<PackedScene>("res://scene.tscn");
+        Generator();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public void Generator()
+    {
+        Random random = new Random();
+
+        byte[] seed = new byte[10];
+        random.NextBytes(seed);
+
+        // Erstelle nur Räume basierend auf den Zufallsbedingungen
+        if (Convert.ToInt64(seed[0].ToString()) < 456)
+        {
+            // Verschiebe den Raum nach oben
+            Vector2I oben = new Vector2I();
+            oben.X = 0;
+            oben.Y = -324;
+            shouldPlacedRoom.Position = lastRoom.Position + oben;
+
+            // Füge den neuen Raum hinzu
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            GD.Print("oben");
+        }
+
+        if (Convert.ToInt64(seed[2].ToString()) <= 0)
+        {
+            // Verschiebe den Raum nach links
+            Vector2I left = new Vector2I();
+            left.X = -576;
+            left.Y = 0;
+            shouldPlacedRoom.Position = lastRoom.Position + left;
+
+            // Füge den neuen Raum hinzu
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            GD.Print("links");
+        }
+
+        if (Convert.ToInt64(seed[2].ToString()) < 0)
+        {
+            // Verschiebe den Raum nach rechts
+            Vector2I right = new Vector2I();
+            right.X = 576;
+            right.Y = 0;
+            shouldPlacedRoom.Position = lastRoom.Position + right;
+
+            // Füge den neuen Raum hinzu
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            GD.Print("right");
+        }
+
+        if (Convert.ToInt64(seed[3].ToString()) < 0)
+        {
+            // Verschiebe den Raum nach unten
+            Vector2I bottom = new Vector2I();
+            bottom.X = 0;
+            bottom.Y = 324;
+            shouldPlacedRoom.Position = lastRoom.Position + bottom;
+
+            // Füge den neuen Raum hinzu
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            GD.Print("bottom");
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
