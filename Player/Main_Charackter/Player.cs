527ca76aca3271adb512b5b287095048619d2fb6
LinusWedauLavie <Linus-Milan.Wedau@bvb.lavie-reha.de>
2024-11-25 14:57:04 +0100

    Pausemenu ingame WIP

--- Player/Main_Charackter/Player.cs
+++ Player/Main_Charackter/Player.cs
@@ -3,16 +3,25 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-
 	[Export] int speed = 250;
+	bool optionMenuShow = false;
 
 	public void GetInput(){
 		Vector2 inputDirection = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
 		Velocity = inputDirection * speed;
+		if (Input.IsActionJustPressed("pauseMenu"))
+		{
+			if()
+			{
+				
+			}
+			//TODO machen das man es auch wieder schließen kann
+		}
 	}
+
     public override void _Ready()
     {
-
+		
     }
 
     public override void _PhysicsProcess(double delta)
