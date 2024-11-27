58f2dd9ffa4ace65171079e8cf949e9454f89ee8
naxissollensterben <zimolngk1@gmail.com>
2024-11-27 16:02:11 +0100

    MapGenerator10RoomsWIP

--- Map/MapGenerator.cs
+++ Map/MapGenerator.cs
@@ -1,8 +1,6 @@
 using Godot;
 using System;
 
-
-
 public partial class MapGenerator : Node2D
 {
     public Node2D lastRoom;
@@ -10,97 +8,89 @@ public partial class MapGenerator : Node2D
     public PackedScene Room1;
     bool[,] roomFieldArray = new bool[40, 40];
     Vector2I lastRoomVector = new Vector2I();
+    int maxRooms = 10;  // maximale anzahl an räumen
+    int roomCount = 0;  //zähler für generierte räume
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         Node2D startRoom = (Node2D)GetParent().GetNode("/root/Map/StartRoom");
         lastRoom = startRoom;
         Room1 = ResourceLoader.Load<PackedScene>("res://Map/Scenes/Room1.tscn");
-        roomFieldArray[19,19] = true;
-        lastRoomVector = new Vector2I(19,19);
+        roomFieldArray[19, 19] = true;  // Startposition
+        lastRoomVector = new Vector2I(19, 19);
         Generator();
     }
 
     public void Generator()
     {
-        Random random = new Random();
+        if (roomCount >= maxRooms)  // checkt ob die maximale anzahl an räumen erreicht wurde
+        {
+            GD.Print("Maximale Raumanzahl erreicht");
+            return;  // Stoppt die Generierung
+        }
 
+        Random random = new Random();
         byte[] seed = new byte[10];
         random.NextBytes(seed);
 
-        // Erstelle und platziere Räume basierend auf Zufallswerten aus dem Seed
-        if (Convert.ToInt64(seed[0].ToString()) < 128) // oben
+        // Versucht, in eine der 4 Richtungen zu generieren
+        if (Convert.ToInt64(seed[0].ToString()) < 128 && !roomFieldArray[lastRoomVector.X - 1, lastRoomVector.Y])  // oben
         {
-            if(roomFieldArray[lastRoomVector.X - 1, lastRoomVector.Y])
-            {
-                //TODO raum existiert da schon, generiere neu
-                GD.Print("neu genereirt oben");
-            }else{
-
-                shouldPlacedRoom = (Node2D)Room1.Instantiate();
-                Vector2I oben = new Vector2I(0, -324);
-                shouldPlacedRoom.Position = oben;
-                lastRoom.AddChild(shouldPlacedRoom);  // Füge den Raum als Kind hinzu
-                lastRoom = shouldPlacedRoom;  // Setze lastRoom auf den neuen Raum
-                GD.Print("oben");
-                roomFieldArray[lastRoomVector.X - 1, lastRoomVector.Y] = true;
-                lastRoomVector.X = lastRoomVector.X -1;
-                
-            }
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            Vector2I oben = new Vector2I(0, -324);
+            shouldPlacedRoom.Position = oben;
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            roomFieldArray[lastRoomVector.X - 1, lastRoomVector.Y] = true;
+            lastRoomVector.X -= 1;
+            roomCount++;
+            GD.Print("Raum nach oben generiert");
         }
 
-        if (Convert.ToInt64(seed[1].ToString()) < 128) // links
+        if (Convert.ToInt64(seed[1].ToString()) < 128 && !roomFieldArray[lastRoomVector.X, lastRoomVector.Y - 1])  // links
         {
-            if(roomFieldArray[lastRoomVector.X, lastRoomVector.Y- 1])
-            {
-                //TODO raum existiert da schon, generiere neu
-                GD.Print("neu genereirt links");
-            }else{
-                shouldPlacedRoom = (Node2D)Room1.Instantiate();
-                Vector2I left = new Vector2I(-576, 0);
-                shouldPlacedRoom.Position =left;
-                lastRoom.AddChild(shouldPlacedRoom);
-                lastRoom = shouldPlacedRoom;
-                GD.Print("links");
-                roomFieldArray[lastRoomVector.X, lastRoomVector.Y- 1] = true;
-                lastRoomVector.Y = lastRoomVector.Y -1;
-            }
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            Vector2I left = new Vector2I(-576, 0);
+            shouldPlacedRoom.Position = left;
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            roomFieldArray[lastRoomVector.X, lastRoomVector.Y - 1] = true;
+            lastRoomVector.Y -= 1;
+            roomCount++;
+            GD.Print("Raum nach links generiert");
         }
 
-        if (Convert.ToInt64(seed[2].ToString()) < 128) // rechts
+        if (Convert.ToInt64(seed[2].ToString()) < 128 && !roomFieldArray[lastRoomVector.X, lastRoomVector.Y + 1])  // rechts
         {
-            if(roomFieldArray[lastRoomVector.X, lastRoomVector.Y + 1])
-            {
-                //TODO raum existiert da schon, generiere neu
-                GD.Print("neu genereirt rechts");
-            }else{
-                shouldPlacedRoom = (Node2D)Room1.Instantiate();
-                Vector2I right = new Vector2I(576, 0);
-                shouldPlacedRoom.Position = right;
-                lastRoom.AddChild(shouldPlacedRoom);
-                lastRoom = shouldPlacedRoom;
-                GD.Print("rechts");
-                roomFieldArray[lastRoomVector.X, lastRoomVector.Y + 1] = true;
-                lastRoomVector.Y = lastRoomVector.Y + 1;
-            }
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            Vector2I right = new Vector2I(576, 0);
+            shouldPlacedRoom.Position = right;
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            roomFieldArray[lastRoomVector.X, lastRoomVector.Y + 1] = true;
+            lastRoomVector.Y += 1;
+            roomCount++;
+            GD.Print("Raum nach rechts generiert");
         }
 
-        if (Convert.ToInt64(seed[3].ToString()) < 128) // unten
+        if (Convert.ToInt64(seed[3].ToString()) < 128 && !roomFieldArray[lastRoomVector.X + 1, lastRoomVector.Y])  // unten
         {
-            if(roomFieldArray[lastRoomVector.X + 1, lastRoomVector.Y])
-            {
-                //TODO raum existiert da schon, generiere neu
-                GD.Print("neu genereirt un  ten");
-            }else{
-                shouldPlacedRoom = (Node2D)Room1.Instantiate();
-                Vector2I bottom = new Vector2I(0, 324);
-                shouldPlacedRoom.Position = bottom;
-                lastRoom.AddChild(shouldPlacedRoom);
-                lastRoom = shouldPlacedRoom;
-                GD.Print("unten");
-                roomFieldArray[lastRoomVector.X + 1, lastRoomVector.Y] = true;
-                lastRoomVector.X = lastRoomVector.X + 1;
-            }
+            shouldPlacedRoom = (Node2D)Room1.Instantiate();
+            Vector2I bottom = new Vector2I(0, 324);
+            shouldPlacedRoom.Position = bottom;
+            lastRoom.AddChild(shouldPlacedRoom);
+            lastRoom = shouldPlacedRoom;
+            roomFieldArray[lastRoomVector.X + 1, lastRoomVector.Y] = true;
+            lastRoomVector.X += 1;
+            roomCount++;
+            GD.Print("Raum nach unten generiert");
+        }
+
+        // Rekursive Generator Ausführung um weitere Räume zu generieren
+        if (roomCount < maxRooms)  
+        {
+            Generator();
         }
     }
 
