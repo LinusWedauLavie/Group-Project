b79ea0498d599971cd2547476324ead96d151ac2
LinusWedauLavie <Linus-Milan.Wedau@bvb.lavie-reha.de>
2024-11-28 10:35:02 +0100

    Player sword bug fix

--- Components/AttackComponentPlayer/AttackComponentPlayer.cs
+++ Components/AttackComponentPlayer/AttackComponentPlayer.cs
@@ -6,19 +6,24 @@ public partial class AttackComponentPlayer : Area2D
 	[Export] int damage;
 	Timer attackTimer;
 	Marker2D pivot;
+	CollisionShape2D attackCollision;
 	bool attackReady = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		attackCollision = GetNode<CollisionShape2D>("CollisionShape2D");
+		attackCollision.Disabled = true;
 		attackTimer = GetNode<Timer>("AttackTimer");
 		pivot = GetParent<Marker2D>();
+
 	}
 
 	public void OnAttackTimerTimeout()
 	{
 		attackReady = true;
 		attackTimer.WaitTime = 0.35;
+		attackCollision.Disabled = true;
 	}
 
 	public void SetWeaponRotation()
@@ -39,6 +44,9 @@ public partial class AttackComponentPlayer : Area2D
 		if(attackReady == true)
 		{
 			attackReady = false;
+			attackCollision.Disabled = false;
+			attackTimer.Start();
+
 		}
 	}
 	public void OnAreaEntered(Area2D area)
@@ -47,7 +55,7 @@ public partial class AttackComponentPlayer : Area2D
 		{
 			enemyHitBox.GiveOwnDamage(damage);
 		}
-		attackTimer.Start();
+		attackCollision.Disabled = true;
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
